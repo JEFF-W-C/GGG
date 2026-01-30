602667cbccd2eefa1b6dd14e6c34ef104451b22a
JEFF-W-C <pkyou12353@gmail.com>
2026-01-30 17:17:41 +0800

    main.js

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ function calcDesignFlow() {
   document.getElementById("designFlowResult").innerText =
     `Design Flow = ${Q.toFixed(1)} LPM`;
 }
+
+window.onload = function () {
+  const el = document.getElementById("versionInfo");
+  el.innerText = `Version ${VERSION_INFO.version} | Last Updated: ${VERSION_INFO.lastUpdated}`;
+};
