81b2124e142c656aab9cea6f2a19f92852ab517a
JEFF-W-C <pkyou12353@gmail.com>
2026-01-30 17:10:24 +0800

    constants.js

--- data/constants.js
+++ data/constants.js
@@ -1,4 +1,9 @@
 const DESIGN = {
-  MAX_CHEM_VELOCITY: 0.3, // m/s (Micron standard)
+  MAX_CHEM_VELOCITY: 0.3, // m/s
   CDA_SPARE_FACTOR: 1.2,
+};
+
+const VERSION_INFO = {
+  version: "v0.1.0",
+  lastUpdated: "2026/01/30 10:45"
 };
