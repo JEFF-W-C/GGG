e60b7c9fce46149d7eddbb8d8451f5d88cc1e3b3
JEFF-W-C <pkyou12353@gmail.com>
2026-04-29 21:56:39 +0800

    Delete modules directory

--- modules/flow.js
+++
@@ -1,7 +0,0 @@
-function getDesignFlow() {
-  const peak = Number(document.getElementById("peakFlow").value);
-  const qty = Number(document.getElementById("toolCount").value);
-  const df = Number(document.getElementById("diversity").value);
-
-  return peak * qty * df; // LPM
-}
